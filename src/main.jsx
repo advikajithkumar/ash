f895ae56640c1716272bed3326fd0d6b75e8bb93
Advik Ajithkumar <advik.ajithkumar@education.nsw.gov.au>
2025-07-31 12:44:13 +1000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // Changed from './App.js'
+import App from './app.jsx' // Changed from './App.js'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
